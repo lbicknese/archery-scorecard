8559f34f47d383e0e4ad338637843c138da36126
Luke Bicknese <luke.bicknese@gmail.com>
2019-11-11 11:13:37 -0600

    e2e parallel and screenshots
    Error added for testing pipeline

---
+++ nightwatch.conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    test_workers: true,
+    screenshots : {
+        enabled : true,
+        on_failure : true,
+        on_error : false,
+        path : './tests/e2e/reports/'
+    }
+}
